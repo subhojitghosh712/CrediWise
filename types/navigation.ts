8e1fbcc64b02047c5a54ba43557e8cca5f52acb5
Subhojit Ghosh <69459860+subhojitghosh712@users.noreply.github.com>
2025-10-19 09:27:42 +0100

    Create navigation.ts

---
+++ types/navigation.ts
@@ -0,0 +1,11 @@
+export type RootStackParamList = {
+  welcomescreen: undefined;
+  learn: undefined;
+  simulator: undefined;
+  quiz: undefined;
+  chatbot: undefined;
+  MainTabs: undefined;
+  compoundscreen: undefined;
+  budgettracker: undefined;
+  loantracker: undefined;
+};
